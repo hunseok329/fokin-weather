04336181527b1f95a114102324eb65fdb6319d1f
hunseok329 <kha5064@gmail.com>
2020-08-03 18:24:06 +0900

    #create getCurrentPosition

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Loading from "./Loading";
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <Loading></Loading>
-  );
-}
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+  getLocation = async() => {
+    try { 
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.setState({isLoading: false})
+    } catch (error){
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render() {
+    const {isLoading} = this.state;
+    return isLoading ? <Loading /> : null;
+    }
+  }
