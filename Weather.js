2a2d9659f678280b5c3385a749546336d36139fe
hunseok329 <kha5064@gmail.com>
2020-09-13 22:34:23 +0900

    add weatherOptions

--- Weather.js
+++ Weather.js
@@ -1,12 +1,66 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from 'expo-linear-gradient';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather({temp}){
+const weatherOptions = {
+    Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Thunderstorm: {
+        iconName: "",
+        gradient: []
+    },
+    Drizzle: {
+        iconName: "",
+        gradient: []
+    },
+    Rain: {
+        iconName: "",
+        gradient: []
+    },
+    Snow: {
+        iconName: "",
+        gradient: []
+    },
+    Atmosphere: {
+        iconName: "",
+        gradient: []
+    },
+    Clear: {
+        iconName: "",
+        gradient: []
+    },
+    Clouds: {
+        iconName: "",
+        gradient: []
+    },
+    Dust: {
+        iconName: "",
+        gradient: []
+    },
+    Mist: {
+        iconName: "",
+        gradient: []
+    }
+};
+
+export default function Weather({temp, condition}){
     return (
-        <View style={styles.container}>
-            <Text>{temp}</Text>
-        </View>
+        <LinearGradient
+                colors={weatherOptions[condition].gradient}
+                style={styles.container}>
+            <StatusBar barStyle="light-content"/>
+            <View style={styles.halfContainer}>
+            <MaterialCommunityIcons size={96} color="white" name={weatherOptions[condition].iconName}/>
+            <Text style={styles.temp}>{temp}o</Text>
+            </View>
+            <View style={styles.halfContainer}>
+
+            </View>
+        </LinearGradient>
     );
 }
 
@@ -17,6 +71,15 @@ Weather.propTypes = {
 
 const styles = StyleSheet.create ({
     container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp:{
+        fontSize:42,
+        color: "white"
+    },
+    halfContainer: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
